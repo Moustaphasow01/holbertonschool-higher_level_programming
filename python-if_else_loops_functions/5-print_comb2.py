0b8994685f2737ca1cf45ac1bde37790562f1985
Moustaphasow01 <105611478+Moustaphasow01@users.noreply.github.com>
2022-09-09 14:26:48 +0200

    fix if syntaxe bug

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for x in range(0,100):
-    if (x!=100)
+    if (x!=100):
     print(" {:02d},".format(x),end="")
-    elif(x==100)
+    elif(x==100):
     print(" {:02d}".format(x))
