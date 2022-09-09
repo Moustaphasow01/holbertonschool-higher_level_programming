726376e577a4fe4f7e6441488fd48bc262d454e3
Moustaphasow01 <105611478+Moustaphasow01@users.noreply.github.com>
2022-09-09 14:39:23 +0200

    fix indentation

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for x in range(0,100):
     if (x!=100):
-    print(" {:02d},".format(x),end="")
-        elif(x==100):
+         print(" {:02d},".format(x),end="")
+    elif(x==100):
         print(" {:02d}".format(x))
