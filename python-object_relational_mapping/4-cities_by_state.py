0d57f72d702d70751ec6804d85ee20fe29d809fe
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2022-11-12 21:37:26 +0100

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-
-"""
-== Safe version / prevents SQL Injection ==
-Script to list all states
-from database hbtn_0e_0usa
-where name of states matches an argument of command line
-"""
+"""Lists all cities by state"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-if "__main__" == __name__:
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        passwd="root",
-        db=sys.argv[3],
-        charset="utf8"
-    )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            ORDER BY cities.id ASC"
-    )
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC;
+        """)
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
