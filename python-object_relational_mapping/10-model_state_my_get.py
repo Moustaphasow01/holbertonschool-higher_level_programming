7948582a64ff05719d898ab377f60752e19fe274
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2022-11-12 21:26:06 +0100

    Update 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Return State object matching arg"""
-
-from sys import argv
+"""
+Start link class to table in database with sqlalchemy
+Script that prints the `State` Object with the 'name' passed
+as argument in command line.
+"""
+import sys
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).\
-        filter(State.name == argv[4]).order_by(State.id).all()
-    if states:
-        print("{}".format(states[0].id))
-    else:
+    found = False
+    for wanted_id in session.query(State).filter(State.name == sys.argv[4]):
+        if wanted_id:
+            found = True
+            print("{:d}".format(wanted_id.id))
+    if found is False:
         print("Not found")
     session.close()
