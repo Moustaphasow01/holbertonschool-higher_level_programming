11156a65a8dfbbbf4daf8df6ab2f78768239ce41
Moustapha yaya sow <sowmoustaphayaya@gmail.com>
2022-11-12 13:47:32 -0800

    file

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Script to list all states
+from database hbtn_0e_0usa
+"""
+
+import MySQLdb
+import sys
+
+if "__main__" == __name__:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
