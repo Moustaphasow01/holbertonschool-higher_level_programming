c043b129f047078ee3ce6e328183699732830969
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2022-11-12 21:51:46 +0100

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-'''script that lists all states from the database '''
-from sys import argv
-import MySQLdb
+"""
+Start link class to table in database with sqlalchemy
+Script that prints the `State` Object with the 'name' passed
+as argument in command line.
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    found = False
+    for wanted_id in session.query(State).filter(State.name == sys.argv[4]):
+        if wanted_id:
+            found = True
+            print("{:d}".format(wanted_id.id))
+    if found is False:
+        print("Not found")
+    session.close()
