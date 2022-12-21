df8c78c6e55ecbd2ca39bb3ae5ccda2ff872cd80
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2022-12-21 12:04:33 +0000

    fixing starwarstitle

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@
   const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
   const request = require('request');
-  request(url, function (error) {
+  request(url, function (error, response) {
     if (error) {
       console.log(error);
     }
