44feb772a379100e1e0167f7873ed6265ff2172a
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2022-12-21 11:59:08 +0100

    update readme with the error condition

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,6 +4,10 @@
 const fs = require('fs');
 const arch = process.argv[2];
 
-
+fs.readFile(arch, 'utf8', (err, jsonString) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
   console.log(jsonString);
 });
