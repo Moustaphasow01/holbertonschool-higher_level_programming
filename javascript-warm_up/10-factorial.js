3e139d25359625a6b0af63638e3fac0bc2658d1f
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2023-01-02 13:38:13 +0000

    Write a script that computes and prints a factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+factoriel
+*/
+
+function facto (a) {
+    let res = 1;
+    for (let i = 0; i < a; i++) {
+      res *= (i + 1);
+    }
+    return res;
+  }
+  console.log(facto(parseInt(process.argv[2])));
